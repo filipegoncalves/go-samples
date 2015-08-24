3b730fa2e2a1df84531bcff8f0eadc41b2f632e8
Filipe Gonçalves <filipe@codinghighway.com>
2015-08-24 22:41:05 +0100

    Updated Fibonacci sequence exercise description

--- fibonacci/fibonacci.go
+++ fibonacci/fibonacci.go
@@ -1,3 +1,11 @@
+/* Taken from the Go Tour (Exercise: Fibonacci closure)
+ *
+ * Let's have some fun with functions.
+ *
+ * Implement a fibonacci function that returns a function (a closure) that returns successive
+ * fibonacci numbers.
+ *
+ */
 
 package main
 
