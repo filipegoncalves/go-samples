bfbcd62dccebd7a0103039a13885300220e8716e
Filipe Gonçalves <filipe@codinghighway.com>
2015-08-24 20:05:08 +0100

    Implemented Fibonacci sequence using closures

---
+++ fibonacci/fibonacci.go
@@ -0,0 +1,19 @@
+
+package main
+
+import "fmt"
+
+func fibonacci() func() int {
+	before_last, last := -1, 1
+	return func() int {
+		last, before_last = before_last+last, last
+		return last
+	}
+}
+
+func main() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
+}
