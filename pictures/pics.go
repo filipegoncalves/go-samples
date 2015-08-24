6c8731f878f5d2ed5a67ea58c8c0e4fcfa8136ca
Filipe Gonçalves <filipe@codinghighway.com>
2015-08-24 19:05:09 +0100

    Updated pictures exercise description

--- pictures/pics.go
+++ pictures/pics.go
@@ -1,3 +1,17 @@
+/* Taken from the Go Tour (Exercise: Slices)
+ *
+ * Implement Pic. It should return a slice of length dy, each element of which is a slice
+ * of dx 8-bit unsigned integers. When you run the program, it will display your picture,
+ * interpreting the integers as grayscale (well, bluescale) values.
+ *
+ * The choice of image is up to you. Interesting functions include (x+y)/2, x*y, and x^y.
+ *
+ * (You need to use a loop to allocate each []uint8 inside the [][]uint8.)
+ *
+ * (Use uint8(intValue) to convert between types.)
+ *
+ */
+
 package main
 
 import "golang.org/x/tour/pic"
