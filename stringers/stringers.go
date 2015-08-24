dd3f76b071acf70af06e180d90d5cce279335adb
Filipe Gonçalves <filipe@codinghighway.com>
2015-08-24 23:02:40 +0100

    Updated stringers exercise (from the Go Tour) description

--- stringers/stringers.go
+++ stringers/stringers.go
@@ -1,3 +1,10 @@
+/* Taken from the Go Tour (Exercise: Stringers)
+ *
+ * Make the IPAddr type implement fmt.Stringer to print the address as a dotted quad.
+ *
+ * For instance, IPAddr{1, 2, 3, 4} should print as "1.2.3.4".
+ *
+ */
 
 package main
 
