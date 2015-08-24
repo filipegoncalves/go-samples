ee005a471477e558844fabf21acc40618523445e
Filipe Gonçalves <filipe@codinghighway.com>
2015-08-24 19:47:34 +0100

    Updated WordCount exercise (from the Go Tour) description

--- word_count/wcount.go
+++ word_count/wcount.go
@@ -1,3 +1,13 @@
+/* Taken from the Go Tour (Exercise: Maps)
+ *
+ * Implement WordCount. It should return a map of the counts of each “word” in the string s.
+ * The wc.Test function runs a test suite against the provided function and prints success or
+ * failure.
+ *
+ * You might find strings.Fields helpful.
+ *
+ */
+
 package main
 
 import (
