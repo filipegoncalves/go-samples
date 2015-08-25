949c5e74e2a30bdc09484fa8ce353069405f2b3f
Filipe Gonçalves <filipe@codinghighway.com>
2015-08-25 19:05:15 +0100

    Added "Readers" exercise from the Go Tour

---
+++ infinite_reader/infreader.go
@@ -0,0 +1,17 @@
+
+package main
+
+import "golang.org/x/tour/reader"
+
+type MyReader struct{}
+
+func (r MyReader) Read(b []byte) (int, error) {
+	for i := range b {
+		b[i] = 'A'
+	}
+	return len(b), nil
+}
+
+func main() {
+	reader.Validate(MyReader{})
+}
