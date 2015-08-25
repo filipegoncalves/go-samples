84d6cf1d58f05b53d21fe1eda84cf04d4e0e51d0
Filipe Gonçalves <filipe@codinghighway.com>
2015-08-25 19:41:22 +0100

    Updated "rot13Reader" exercise description (from the Go Tour)

--- rot13reader/rot13.go
+++ rot13reader/rot13.go
@@ -1,3 +1,16 @@
+/* Taken from the Go Tour (Exercise: rot13Reader)
+ *
+ * A common pattern is an io.Reader that wraps another io.Reader, modifying the stream in some way.
+ *
+ * For example, the gzip.NewReader function takes an io.Reader (a stream of compressed data) and
+ * returns a *gzip.Reader that also implements io.Reader (a stream of the decompressed data).
+ *
+ * Implement a rot13Reader that implements io.Reader and reads from an io.Reader, modifying the
+ * stream by applying the rot13 substitution cipher to all alphabetical characters.
+ *
+ * The rot13Reader type is provided for you. Make it an io.Reader by implementing its Read method.
+ *
+ */
 
 package main
 
