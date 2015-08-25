575c1d9ec0bb4c20b2aaf7faa5e76e681aa4f208
Filipe Gonçalves <filipe@codinghighway.com>
2015-08-26 00:06:56 +0100

    Updated description for the "HTTP Handlers" exercise (from the Go Tour)

--- http_handlers/http_handlers.go
+++ http_handlers/http_handlers.go
@@ -1,3 +1,25 @@
+/* Taken from the Go Tour (Exercise: HTTP Handlers)
+ *
+ * Implement the following types and define ServeHTTP methods on them. Register them to handle
+ * specific paths in your web server.
+ *
+ * type String string
+ *
+ * type Struct struct {
+ *    Greeting string
+ *    Punct    string
+ *    Who      string
+ * }
+ *
+ * For example, you should be able to register handlers using:
+ *
+ * http.Handle("/string", String("I'm a frayed knot."))
+ * http.Handle("/struct", &Struct{"Hello", ":", "Gophers!"})
+ *
+ * Note: This example won't run through the web-based tour user interface. To try writing web
+ * servers you may want to Install Go.
+ *
+ */
 
 package main
 
