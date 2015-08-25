1b7170b6ecdda04d66b218294d86d1ab3a665a70
Filipe Gonçalves <filipe@codinghighway.com>
2015-08-26 00:02:49 +0100

    Added index page with links for /struct and /string in the HTTP Handler exercise (Go Tour)

--- http_handlers/http_handlers.go
+++ http_handlers/http_handlers.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 )
 
+type MyServer struct{}
+
 type String string
 
 type Struct struct {
@@ -28,9 +30,19 @@ func (s String) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, s)
 }
 
+func (s MyServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	const index = "<h1>It works!</h1>" +
+		"(Blatantly stolen from the Apache default page)<br /><br />" +
+		"<p>See <a href=\"/string\">/string</a> to invoke the String HTTP Handler</p>" +
+		"<p>See <a href=\"/struct\">/struct</a> to invoke the Struct HTTP Handler</p>"
+	fmt.Fprint(w, index)
+}
+
 func main() {
+	var server MyServer
 	http.Handle("/string", String("I'm a frayed knot."))
 	http.Handle("/struct", &Struct{"Hello", ":", "Gophers!"})
+	http.Handle("/", server)
 	if e := http.ListenAndServe("localhost:4000", nil); e != nil {
 		fmt.Println("Error on http.ListenAndServe:", e)
 		os.Exit(1)
