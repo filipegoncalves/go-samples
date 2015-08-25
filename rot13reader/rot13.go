ee944ef312e00648ad8da1ee36be52f2fa67553b
Filipe Gonçalves <filipe@codinghighway.com>
2015-08-25 19:41:44 +0100

    Added explanation about error handling on the "rot13Reader" exercise

--- rot13reader/rot13.go
+++ rot13reader/rot13.go
@@ -24,6 +24,29 @@ type rot13Reader struct {
 	r io.Reader
 }
 
+/* A note about error handling here:
+ *
+ * According to the documentation for the Reader interface, callers of the Read() method
+ * should always process the n > 0 number of bytes returned before processing a (possible)
+ * error status. This is the correct behavior because if there was an error, we still want
+ * to rot13 the part of the stream that was successfully read (and if there was an error before
+ * anything could be read at all, n will be 0 so we won't process anything).
+ *
+ * Here's the relevant quote from the documentation regarding the Read() method of the 
+ * io.Reader interface (see http://golang.org/pkg/io/#Reader):
+ *
+ * When Read encounters an error or end-of-file condition after successfully reading n > 0 bytes, it
+ * returns the number of bytes read. It may return the (non-nil) error from the same call or return
+ * the error (and n == 0) from a subsequent call. An instance of this general case is that a Reader
+ * returning a non-zero number of bytes at the end of the input stream may return either err == EOF
+ * or err == nil. The next Read should return 0, EOF.
+ *
+ * Callers should always process the n > 0 bytes returned before considering the error err. Doing
+ * so correctly handles I/O errors that happen after reading some bytes and also both of the allowed
+ * EOF behaviors.
+ *
+ */
+
 func (rotr rot13Reader) Read(b []byte) (int, error) {
 	n, e := rotr.r.Read(b)
 	for i := 0; i < n; i++ {
