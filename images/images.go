89cc18e639b27ab1e5db0d00a8eddc9d481b00a7
Filipe Gonçalves <filipe@codinghighway.com>
2015-08-26 15:28:06 +0100

    Updated "Images" exercise description (from the Go Tour)

--- images/images.go
+++ images/images.go
@@ -1,3 +1,18 @@
+/* Taken from the Go Tour (Exercise: Images)
+ *
+ * Remember the picture generator you wrote earlier? Let's write another one, but this time it will
+ * return an implementation of image.Image instead of a slice of data.
+ *
+ * Define your own Image type, implement the necessary methods, and call pic.ShowImage.
+ *
+ * Bounds should return a image.Rectangle, like image.Rect(0, 0, w, h).
+ *
+ * ColorModel should return color.RGBAModel.
+ *
+ * At should return a color; the value v in the last picture generator corresponds to
+ * color.RGBA{v, v, 255, 255} in this one.
+ */
+
 package main
 
 import (
