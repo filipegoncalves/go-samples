cf619614bd98f42b6e5d16313898f776a7e8d7e4
Filipe Gonçalves <filipe@codinghighway.com>
2015-08-26 22:12:08 +0100

    Updated "Equivalent Binary Trees" exercise description (from the Go Tour)

--- equivalent_btrees/btrees.go
+++ equivalent_btrees/btrees.go
@@ -1,3 +1,50 @@
+/* Taken from the Go Tour (Exercise: Equivalent Binary Trees)
+ *
+ * There can be many different binary trees with the same sequence of values stored at the leaves.
+ * For example, here are two binary trees storing the sequence 1, 1, 2, 3, 5, 8, 13.
+ *
+ *                                    8
+ *         3                         / \
+ *       /   \                      3   13
+ *      /     \                   /   \
+ *     /       \                 /     \
+ *    1         8               1       5
+ *  /   \     /   \           /   \
+ * 1     2   5    13         1     2
+ *
+ * A function to check whether two binary trees store the same sequence is quite complex in most
+ * languages. We'll use Go's concurrency and channels to write a simple solution.
+ *
+ * This example uses the tree package, which defines the type:
+ *
+ * type Tree struct {
+ *    Left  *Tree
+ *    Value int
+ *    Right *Tree
+ * }
+ *
+ * 1. Implement the Walk function.
+ *
+ * 2. Test the Walk function.
+ *
+ * The function tree.New(k) constructs a randomly-structured binary tree holding the
+ * values k, 2k, 3k, ..., 10k.
+ *
+ * Create a new channel ch and kick off the walker:
+ *
+ * go Walk(tree.New(1), ch)
+ *
+ * Then read and print 10 values from the channel. It should be the numbers 1, 2, 3, ..., 10.
+ *
+ * 3. Implement the Same function using Walk to determine whether t1 and t2 store the same values.
+ *
+ * 4. Test the Same function.
+ *
+ * Same(tree.New(1), tree.New(1)) should return true, and Same(tree.New(1), tree.New(2)) should
+ * return false.
+ *
+ */
+
 package main
 
 import (
