a8ccd53078b26c36805f16ee2096fcd0b7944a30
Filipe Gonçalves <filipe@codinghighway.com>
2015-08-26 19:45:48 +0100

    Added description for the multithreaded sum example in Go

--- threaded_sum/sum.go
+++ threaded_sum/sum.go
@@ -1,3 +1,16 @@
+/* This code snippet was taken from the Go Tour.
+ *
+ * It shows how easy it is to do simple multithreaded tasks in Go.
+ *
+ * This particular example fires up a pair of goroutines that concurrently
+ * sum each half of a slice and write the result into a channel. The results
+ * are read from the channel in the main thread and combined together to produce
+ * the final result.
+ *
+ * It is educational and very interesting to see how simple and brief the code is when
+ * compared to, say, C or C++
+ *
+ */
 
 package main
 
