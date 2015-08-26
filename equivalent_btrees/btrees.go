e5c00c4097221c6ae5af361e4b5fae637966dee0
Filipe Gonçalves <filipe@codinghighway.com>
2015-08-26 21:46:34 +0100

    Implementing and testing Walk() for the "Equivalent Binary Trees" exercise.

---
+++ equivalent_btrees/btrees.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"golang.org/x/tour/tree"
+	"fmt"
+)
+
+// Walk walks the tree t sending all values
+// from the tree to the channel ch.
+func Walk(t *tree.Tree, ch chan int) {
+	if t == nil {
+		return	
+	}
+	Walk(t.Left, ch)
+	ch <- t.Value
+	Walk(t.Right, ch)
+}
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+//func Same(t1, t2 *tree.Tree) bool
+
+func main() {
+	ch := make(chan int)
+	go Walk(tree.New(1), ch)
+	for i := 0; i < 10; i++ {
+		fmt.Println(<- ch)
+	}
+}
