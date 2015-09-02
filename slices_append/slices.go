508fb497a90dfef592f1dd8735c38221e7523d72
Filipe Gonçalves <filipe@codinghighway.com>
2015-09-02 22:19:47 +0100

    Added error handling for "slices append"

--- slices_append/slices.go
+++ slices_append/slices.go
@@ -8,15 +8,26 @@ import (
 
 var digitRegexp = regexp.MustCompile("[0-9]+")
 
-func FindDigits(filename string) []byte {
-	b, _ := ioutil.ReadFile(filename)
+func FindDigits(filename string) ([]byte, error) {
+	b, e := ioutil.ReadFile(filename)
+
+	if e != nil {
+		return b, e
+	}
+
 	b = digitRegexp.Find(b)
 	c := make([]byte, len(b))
 	copy(c, b)
-	return c
+
+	return c, nil
 }
 
 func main() {
-	d := FindDigits("sample.in")
-	fmt.Printf("Digits found: %s\n", d)
+	d, e := FindDigits("sample.in")
+
+	if (e != nil) {
+		fmt.Printf("Error: %v\n", e)
+	} else {
+		fmt.Printf("Digits found: %s\n", d)
+	}
 }
