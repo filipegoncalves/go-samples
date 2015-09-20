e869b921cc963e0bab66ebe510ea09f85d4de742
Filipe Goncalves <filipe@codinghighway.com>
2015-09-20 17:16:39 +0100

    Added "fan in" concurrency pattern

---
+++ concurrency_patterns/fanin/fanin.go
@@ -0,0 +1,38 @@
+
+package main
+
+import (
+	"fmt"
+	"time"
+	"math/rand"
+)
+
+func boring(msg string) <- chan string {
+	c := make(chan string)
+	go func() {
+		for i := 0; ; i++ {
+			c <- fmt.Sprintf("%s %d", msg, i)
+			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+		}
+	}()
+	return c
+}
+
+func fanIn(input1, input2 <- chan string) <- chan string {
+	c := make(chan string)
+	go func() { for { c <- <-input1 } }()
+	go func() { for { c <- <-input2 } }()
+	return c
+}
+
+func main() {
+	joe := boring("joe")
+	ann := boring("ann")
+
+	for i := 0; i < 5; i++ {
+		fmt.Println(<- joe)
+		fmt.Println(<- ann)
+	}
+
+	fmt.Println("You're boring, I'm leaving.")
+}
