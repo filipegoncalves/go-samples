0fb84ae796f552bd648f9ff96d9269981708b65e
Filipe Goncalves <filipe@codinghighway.com>
2015-09-20 17:54:14 +0100

    Added chinese whispers game example

---
+++ concurrency_patterns/chinese_whispers/whispers.go
@@ -0,0 +1,26 @@
+
+package main
+
+import (
+	"fmt"
+)
+
+func say(right, left chan int) {
+	left <- 1 + <- right
+}
+
+func main() {
+	const gophers = 100000
+	rightmost := make(chan int)
+	right := rightmost
+	var left chan int
+
+	for i := 0; i < gophers; i++ {
+		left = make(chan int)
+		go say(right, left)
+		right = left
+	}
+
+	rightmost <- 1
+	fmt.Println(<- left)
+}
